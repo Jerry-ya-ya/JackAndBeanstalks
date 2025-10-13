2709e70857e2a754c6f003b98630761a0211cb57
Yee-Bee-Ya-Ya <Platypus.0905311287@gmail.com>
2025-10-13 11:54:27 +0800

    Preparing to refactor the backend to replace Flask with Java.

--- Backend/redis/redis_client.py
+++
@@ -1,6 +0,0 @@
-import os
-import redis
-
-# 統一由環境變數 REDIS_URL 控制，預設為 docker 服務名 'redis'
-_redis_url = os.getenv('REDIS_URL', 'http://localhost:6379/0')
-r = redis.from_url(_redis_url, decode_responses=True)
